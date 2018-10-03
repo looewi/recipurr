8dc2e7b865a45eb4b635127c6c4bee239235775f
Bruno Oliveira <bruno.oliveira@mdcpartners.be>
2018-10-03 11:53:44 +0200

    Adding serialization to data class for queue

--- recipe-crawler/src/main/kotlin/com/recipurr/recipecrawler/service/queue/internal/UrlQueueItem.kt
+++ recipe-crawler/src/main/kotlin/com/recipurr/recipecrawler/service/queue/internal/UrlQueueItem.kt
@@ -1,3 +1,3 @@
 package com.recipurr.recipecrawler.service.queue.internal
 
-data class UrlQueueItem(val url: String, val type: String)
+data class UrlQueueItem(val url: String, val type: String) : java.io.Serializable
