24e1b24a9c128116fb5fe2a9dd760cb1897dc8d3
Bruno Oliveira <bruno.oliveira@mdcpartners.be>
2018-10-03 13:09:42 +0200

    Receiver for Queue

---
+++ recipe-crawler/src/main/kotlin/com/recipurr/recipecrawler/service/queue/internal/UrlQueueReceiver.kt
@@ -0,0 +1,9 @@
+package com.recipurr.recipecrawler.service.queue.internal
+
+
+class UrlQueueReceiver {
+
+    fun processQueueItems(links: List<UrlQueueItem>) {
+
+    }
+}
--- recipe-crawler/src/main/kotlin/com/recipurr/recipecrawler/service/queue/internal/UrlQueueSender.kt
+++ recipe-crawler/src/main/kotlin/com/recipurr/recipecrawler/service/queue/internal/UrlQueueSender.kt
@@ -1,9 +1,6 @@
 package com.recipurr.recipecrawler.service.queue.internal
 
-import org.springframework.stereotype.Service
 
-
-@Service
 class UrlQueueSender {
 
     fun addLinksToQueue(links: List<UrlQueueItem>) {
