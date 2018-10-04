eb6344d2831962a050dc058089b8f07e41c88a22
looewi <kuchumova.mary@gmail.com>
2018-10-04 11:51:16 +0300

    meow <3

--- recipe-crawler/src/main/kotlin/com/recipurr/recipecrawler/configuration/RabbitQueueConfiguration.kt
+++ recipe-crawler/src/main/kotlin/com/recipurr/recipecrawler/configuration/RabbitQueueConfiguration.kt
@@ -1,16 +1,64 @@
 package src.main.kotlin.com.recipurr.recipecrawler.configuration
 
-@Profile("!usage_message")
+import com.recipurr.recipecrawler.service.queue.internal.UrlQueueReceiver
+import com.recipurr.recipecrawler.service.queue.internal.UrlQueueSender
+import org.springframework.amqp.core.Queue
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+// it'll be better to rename configuration as UrlQueueConfiguration. the same is related to queue, sender, receiver, etc
+// we'll have a lot of other queues. it's better to use different files for their configuration
+// 1) it's following principle of decoupling code and single-responsibility code
+// 2) it's easier to navigate and modify code
+// 3) it's easier and safer to remove dependencies which are not needed anymore
+//
+// so instead of
+// @Configuration
+// class RabbitQueueConfiguration {
+//
+// let's do it like (also it's needed to rename a file, but I'll leave it like that for now so you can see just modifications in github
 @Configuration
-class RabbitQueueConfiguration {
-    @Bean
-    fun hello() = Queue("hello")
+class UrlQueueConfiguration {
 
-    @Profile("receiver")
-    @Bean
-    fun receiver() = Receiver()
+    //    @Bean
+    //    fun hello() = Queue("hello")
+    // beans are accessed in other classes by name. we don't need bean hello which is actually a queue
+    // be careful to name beans exactly as they will be used in other classes <3
 
-    @Profile("sender")
+    // also it'll be better to configure queue name from .yml configuration but we can leave a
+    // hardcoded "hello" value for now
     @Bean
-    fun sender(template: RabbitTemplate, queue: Queue) = Sender(template, queue)
+    fun urlQueue() = Queue("hello")
+
+    @Bean
+    fun urlTemplate() = RabbitTemplate()
+
+
+    //    @Profile("receiver") <- and no profiles
+    //    @Bean
+    //    fun receiver() = Receiver()
+    //
+    // ofc class Receiver can't be found
+    // this is not a class from a library, this is our own class which is responsible for receiving messages
+    // so in our case it'll be UrlQueueReceiver
+    // so code will be
+    @Bean
+    fun urlQueueReceiver() = UrlQueueReceiver()
+
+
+    //    @Profile("sender")
+    //    @Bean
+    //    fun sender(template: RabbitTemplate, queue: Queue) = Sender(template, queue)
+    //
+    // the same with sender
+    // BUT also
+    // sender(template: RabbitTemplate, queue: Queue)
+    // you see that we need to pass RabbitTemplate and Queue as dependencies
+    // so you also need to create them as beans otherwise spring won't find autowired objects of this type
+    // bean for sender will look like this (queue is created in line 33, template - line 37)
+    @Bean
+    fun urlQueueSender(urlQueue: Queue, urlTemplate: RabbitTemplate) = UrlQueueSender(urlQueue, urlTemplate)
+
 }
--- recipe-crawler/src/main/kotlin/com/recipurr/recipecrawler/service/queue/internal/UrlQueueSender.kt
+++ recipe-crawler/src/main/kotlin/com/recipurr/recipecrawler/service/queue/internal/UrlQueueSender.kt
@@ -1,7 +1,14 @@
 package com.recipurr.recipecrawler.service.queue.internal
 
+import org.springframework.amqp.core.Queue
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Autowired
 
-class UrlQueueSender {
+
+// we need dependencies for the queue so let's add it here to the class constructor
+// bean 'urlQueueSender' is an instance of this class so it'll accept these parameters to constructor
+class UrlQueueSender(@Autowired urlQueue: Queue,
+                     @Autowired urlTemplate: RabbitTemplate) {
 
     fun addLinksToQueue(links: List<UrlQueueItem>) {
 
